3a14c1c06354ebc80a02b55eb8e5ec506342c455
Ivan Sukhanov <sukhanov12@yandex.ru>
2025-11-20 12:16:39 +0400

    Разбил на файлы

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  vueIndentScriptAndStyle: true,
+  printWidth: 80,
+  endOfLine: 'lf',
+};
